897a776f22e1f2577c43176e81203180c43ac4bf
Odunmbaku Eniola <eniolacodes1@gmail.com>
2025-06-16 05:37:26 +0100

    complete authentication

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const redis = new Redis({
   host: process.env.REDIS_HOST as string,
   port: process.env.REDIS_PORT as number | undefined,
+  password: process.env.REDIS_PASSWORD
 });
 
 export default redis;
