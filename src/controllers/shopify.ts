9d8b4c4637d3d4d854819792d39d0157e94507cd
Eniola Odunmbaku <eniolacodes1@gmail.com>
2026-03-30 16:59:46 +0100

    Shopify implementation (#15)
    * shopify implementation changes

--- src/controllers/shopify.ts
+++ src/controllers/shopify.ts
@@ -1,25 +1,21 @@
-import { NextFunction, Request, Response } from "express";
-import { SHOPIFY } from "../lib/constants/endpoints";
-import shopify from "../services/shopify";
+import { NextFunction, Request, Response } from 'express';
+import { SHOPIFY } from '../lib/constants/endpoints';
+import shopify from '../services/shopify';
 
- export const init = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("Hi")
+export const init = async (req: Request, res: Response, next: NextFunction) => {
   try {
-  const response = await shopify.auth.begin({
+    await shopify.auth.begin({
       shop: shopify.utils.sanitizeShop(req.query.shop as string, true)!,
       callbackPath: `${SHOPIFY.base}${SHOPIFY.branches.redirect}`,
       isOnline: false,
       rawRequest: req,
       rawResponse: res,
     });
-
-    console.log(response)
-    console.log("Hi2")
-  } catch (error) {
+  } catch (error: any) {
+    if (!error.statusCode) error.statusCode = 500;
     next(error);
-    console.log(error)
-    console.log("Hi3")
-  }};
+  }
+};
 
 export const redirect = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -30,14 +26,12 @@ export const redirect = async (req: Request, res: Response, next: NextFunction) 
 
     const { shop, accessToken } = callback.session;
 
-    console.log('Access token for', shop, ':', accessToken);
-
-    // TODO: save session to your database here
-    // await db.saveSession(shop, accessToken);
-
-    // Redirect user back to your frontend
-    res.redirect(302, `${process.env.FRONTEND_URL}/dashboard?shop=${shop}`);
-  } catch (error) {
+    res.redirect(
+      302,
+      `${process.env.FRONTEND_APPLICATION_URL}/dashboard?shop=${shop}&token=${accessToken}`
+    );
+  } catch (error: any) {
+    if (!error.statusCode) error.statusCode = 500;
     next(error);
   }
 };
